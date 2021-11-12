dc496a3506e8c9bc3b43d6598fc39c60292b1661
Graeme Brown <graemebrown1874@gmail.com>
2021-11-12 16:38:08 +0000

    added run_sql file and did boiler plate for creation of tables

---
+++ db/store_inventories.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS species;
+DROP TABLE IF EXISTS subcategory;
+DROP TABLE IF EXISTS category;
+
+CREATE TABLE category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE subcategory (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    category_id INT REFERENCES category(id)
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    difficulty VARCHAR(255),
+    stock_no INT,
+    buying_price INT,
+    selling_price INT,
+    subcategory_id INT REFERENCES subcategory(id)
+);
